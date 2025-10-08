54dbc83ca6404a62d43ed4f43058813833401f8c
Ayush Kumar Srivastava <167962127+Astrio12345@users.noreply.github.com>
2025-10-09 00:13:38 +0530

    Initial commit - added Single Number program
    Single Number
    
    - (Ques. No. 136)
    
    - https://leetcode.com/problems/single-number/description/

---
+++ Single Number/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+        System.out.println(singleNumber(nums));
+    }
+}
